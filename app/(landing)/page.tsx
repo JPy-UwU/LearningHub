059b031008699df65920360f5ca42fe68e568123
JP <jay.patel.py@gmail.com>
2023-09-21 20:23:12 -0600

    Added sign-in and sign-up buttons for landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
 const LandingPage = () => {
   return (
     <div>
       Landing Page
+      <div>
+        <Link href="/sign-up">
+          <Button>
+            Sign Up
+          </Button>
+        </Link>
+        <Link href="/sign-in">
+          <Button>
+            Sign In
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
