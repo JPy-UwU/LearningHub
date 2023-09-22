373e17ac49cf18376d4687e1b4c459cf3b37f152
JP <jay.patel.py@gmail.com>
2023-09-22 17:31:32 -0600

    Added sidebar - added sidebar component - added dashboard layout

---
+++ app/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+  return (
+    <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
+      Sidebar
+    </div>
+  );
+}
+ 
+export default Sidebar;
