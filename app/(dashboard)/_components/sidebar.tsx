2d1142ff081327a1a2eb1ed53e4c408e0bb5c1f8
JP <jay.patel.py@gmail.com>
2023-09-22 20:25:25 -0600

    Added Logo component - Added logo placeholder

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -1,6 +1,9 @@
+import Logo from "./Logo";
+
 const Sidebar = () => {
   return (
     <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
+      <Logo />
       Sidebar
     </div>
   );
