f5a6db546781d6aacc5fd5cfcfe8ccdbe35a9ae8
JP <jay.patel.py@gmail.com>
2023-09-22 20:26:23 -0600

    Added padding for logo

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -3,7 +3,9 @@ import Logo from "./Logo";
 const Sidebar = () => {
   return (
     <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
-      <Logo />
+      <div className="p-6">
+        <Logo />
+      </div>
       Sidebar
     </div>
   );
