404832aa15e721c754e71b07b916038c34fd8459
JP <jay.patel.py@gmail.com>
2023-09-24 13:00:07 -0600

    Added navbar-routes component - the component contains user button for now

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,9 +1,11 @@
+import NavbarRoutes from "@/components/navbar-routes";
 import MobileSidebar from "./mobile-sidebar";
 
 const Navbar = () => {
   return (
     <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
       <MobileSidebar />
+      <NavbarRoutes />
     </div>
   );
 }
