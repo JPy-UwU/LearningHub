006160ac7e6888c10d1183dbafae3c11c22549f1
JP <jay.patel.py@gmail.com>
2023-09-24 14:16:01 -0600

    Updated exit button link

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -2,14 +2,13 @@
 
 import { LogOut } from "lucide-react";
 import { UserButton } from "@clerk/nextjs";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 import { Button } from "./ui/button";
 
 const NavbarRoutes = () => {
   const pathname = usePathname();
-  const router = useRouter();
 
   const isAdminPage = pathname?.startsWith("/admin");
   const isPlayerPage = pathname?.includes("/chapter");
@@ -17,10 +16,12 @@ const NavbarRoutes = () => {
   return (
     <div className="flex gap-2 ml-auto">
       {isAdminPage || isPlayerPage ?  (
-        <Button size="sm" variant="ghost">
-          <LogOut className="h-4 w-4 mr-2" />
-          Exit
-        </Button>
+        <Link href="/dashboard">
+          <Button size="sm" variant="ghost">
+            <LogOut className="h-4 w-4 mr-2" />
+            Exit
+          </Button>
+        </Link>
       ): (
         <Link href="/admin/courses">
           <Button size="sm" variant="ghost">
