a735afbf1c3adf73b01af6733adce23e0f870ae2
JP <jay.patel.py@gmail.com>
2023-09-24 12:55:08 -0600

    Changed sidebar to be responsive

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,6 +1,9 @@
+import MobileSidebar from "./mobile-sidebar";
+
 const Navbar = () => {
   return (
     <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+      <MobileSidebar />
     </div>
   );
 }
