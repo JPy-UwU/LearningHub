2c42420ad2e64886a1b4a3191a6035fbbd4b106b
JP <jay.patel.py@gmail.com>
2023-09-24 13:51:12 -0600

    Added admin mode and exit button for navbar

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,11 +1,34 @@
 "use client";
 
+import { LogOut } from "lucide-react";
 import { UserButton } from "@clerk/nextjs";
+import { usePathname, useRouter } from "next/navigation";
+import Link from "next/link";
+
+import { Button } from "./ui/button";
 
 const NavbarRoutes = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const isAdminPage = pathname?.startsWith("/admin");
+  const isPlayerPage = pathname?.includes("/chapter");
+
   return (
     <div className="flex gap-2 ml-auto">
-      <UserButton />
+      {isAdminPage || isPlayerPage ?  (
+        <Button size="sm" variant="ghost">
+          <LogOut className="h-4 w-4 mr-2" />
+          Exit
+        </Button>
+      ): (
+        <Link href="/admin/courses">
+          <Button size="sm" variant="ghost">
+            Admin Mode
+          </Button>
+        </Link>
+      )}
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
