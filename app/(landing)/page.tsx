66a791b23bc9fdfb021dbf568daa5a13d23024c6
JP <jay.patel.py@gmail.com>
2023-09-25 06:06:31 -0600

    Added check for signed in users - signed in users are now redirected to dashboard

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -3,22 +3,26 @@
  * Description: A landing page for the website, contanins sign-up and sign-in links
  */
 
+"use client";
 
 import Link from "next/link";
+import { useAuth } from "@clerk/nextjs";
 
 import { Button } from "@/components/ui/button";
 
 const LandingPage = () => {
+  const { isSignedIn } = useAuth();
+
   return (
     <div>
       Landing Page
       <div>
-        <Link href="/sign-up">
+        <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
           <Button>
             Sign Up
           </Button>
         </Link>
-        <Link href="/sign-in">
+        <Link href={isSignedIn ? "/dashboard" : "/sign-in"}>
           <Button>
             Sign In
           </Button>
