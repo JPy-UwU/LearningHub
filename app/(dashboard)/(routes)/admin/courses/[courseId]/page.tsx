4ca80623b0ff4a2b73982826363460bca8570711
JP <jay.patel.py@gmail.com>
2023-10-11 16:51:33 -0600

    Added database connection for course creation

--- app/(dashboard)/(routes)/admin/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/admin/courses/[courseId]/page.tsx
@@ -1,10 +1,43 @@
-const CourseIdPage = ({
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const CourseIdPage = async ({
   params
 }: {
   params: { courseId: string }
 }) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/login");
+  }
+
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId
+    }
+  });
+
+  if (!course) {
+    return redirect("/dashboard");
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+  ];
+
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+
+  const completionTest = `${completedFields}/${totalFields} fields completed.`;
+
   return (
-    <div>
+    <div className="p-6">
       CourseIdPage
     </div>
   );
