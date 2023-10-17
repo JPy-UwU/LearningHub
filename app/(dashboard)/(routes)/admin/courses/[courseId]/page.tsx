64c4a9019d54a86e35bfe2e2bb7075f643c8ee6f
JP <jay.patel.py@gmail.com>
2023-10-17 15:33:41 -0600

    Added page for course title - added cn variants for ui compontents

--- app/(dashboard)/(routes)/admin/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/admin/courses/[courseId]/page.tsx
@@ -1,6 +1,10 @@
-import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { LayoutDashboard } from "lucide-react";
+
+import { IconBadge } from "@/components/icon-badge";
+import { TitleForm } from "./_components/title-form";
+import { db } from "@/lib/db";
 
 const CourseIdPage = async ({
   params
@@ -38,7 +42,30 @@ const CourseIdPage = async ({
 
   return (
     <div className="p-6">
-      CourseIdPage
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">
+            Course Setup
+          </h1>
+          <span className="text-sm text-slate-700">
+            Complete all fields. {completionTest}
+          </span>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard} />
+            <h2 className="text-xl">
+              Customize your course
+            </h2>
+          </div>
+          <TitleForm
+            initialData={course}
+            courseId={course.id}
+          />
+        </div>
+      </div>
     </div>
   );
 }
