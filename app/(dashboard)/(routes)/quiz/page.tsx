752a2db58dda22ee501e8f150e32be644ee8e678
ibrahim11235 <ibrahimtouqani@gmail.com>
2023-10-17 13:55:52 -0600

    Adding loading page wating DB

--- app/(dashboard)/(routes)/quiz/page.tsx
+++ app/(dashboard)/(routes)/quiz/page.tsx
@@ -13,11 +13,6 @@ export const metadata = {
 };
 
 const Dasboard = async (props: Props) => {
-  const session = await getAuthSession();
-  if (!session?.user) {
-    redirect("/");
-  }
-  
 
   return (
     <main className="p-8 mx-auto max-w-7xl">
