4bc3dad9f0250805349eee8a48bbc2aeb98e4c90
JP <jay.patel.py@gmail.com>
2023-10-21 10:33:56 -0600

    uncommented code

--- app/api/game/route.ts
+++ app/api/game/route.ts
@@ -49,60 +49,7 @@ export async function POST(req: Request, res: Response) {
       },
     });
 
-    // const { data } = await axios.post(
-    //   `/api/questions`,
-    //   {
-    //     amount,
-    //     topic,
-    //     type,
-    //   }
-    // );
-
-    // if (type === "mcq") {
-    //   type mcqQuestion = {
-    //     question: string;
-    //     answer: string;
-    //     option1: string;
-    //     option2: string;
-    //     option3: string;
-    //   };
-
-    //   const manyData = data.questions.map((question: mcqQuestion) => {
-    //     // mix up the options lol
-    //     const options = [
-    //       question.option1,
-    //       question.option2,
-    //       question.option3,
-    //       question.answer,
-    //     ].sort(() => Math.random() - 0.5);
-    //     return {
-    //       question: question.question,
-    //       answer: question.answer,
-    //       options: JSON.stringify(options),
-    //       gameId: game.id,
-    //       questionType: "mcq",
-    //     };
-    //   });
-
-    //   await prisma.question.createMany({
-    //     data: manyData,
-    //   });
-    // } else if (type === "open_ended") {
-    //   type openQuestion = {
-    //     question: string;
-    //     answer: string;
-    //   };
-    //   await prisma.question.createMany({
-    //     data: data.questions.map((question: openQuestion) => {
-    //       return {
-    //         question: question.question,
-    //         answer: question.answer,
-    //         gameId: game.id,
-    //         questionType: "open_ended",
-    //       };
-    //     }),
-    //   });
-    // }
+    ///
 
     return NextResponse.json({ gameId: game.id }, { status: 200 });
   } catch (error) {
