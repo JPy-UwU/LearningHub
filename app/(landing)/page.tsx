cbf8a16c93404528e8fd81940347d71ed7678ef9
JP <jay.patel.py@gmail.com>
2023-11-15 13:03:55 -0600

    Remove NavBar and update Landing Page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,30 +1,8 @@
-// page.tsx
-
-import Link from "next/link";
-import { useAuth } from "@clerk/nextjs";
-import { Button } from "@/components/ui/button";
-import NavBar from './NavBar';
-import LibraryBanner from './LibraryBanner';
-import WhyUs from './WhyUs';
 const LandingPage = () => {
 
   return (
     <div>
-      <NavBar />
-      <LibraryBanner />
-      <WhyUs /> 
-      {/* <div className="flex justify-center space-x-4 mt-10">
-        <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
-          <Button>
-            Sign Up
-          </Button>
-        </Link>
-        <Link href={isSignedIn ? "/dashboard" : "/sign-in"}>
-          <Button>
-            Sign In
-          </Button>
-        </Link>
-      </div> */}
+      Landing Page
     </div>
   );
 }
