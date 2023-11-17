260337946007ea1658c19e977c78ee925c764ce4
krutik <szd467@usask.ca>
2023-11-17 12:53:40 -0600

    work doen till userProgress

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,4 +1,17 @@
-const ChapterIdPage = () => {
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const ChapterIdPage = async ({
+  params
+}: {
+  params: {courseId: string; chapterId: string}
+}) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/sign-in");
+  }
+
   return ( 
     <div>
       Chapter Id
